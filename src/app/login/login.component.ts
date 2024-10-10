c05a08c1064670b109fab28c70818a2c2c27724c
Alan Gonzalo Ramos <alanramos_18@hotmail.com>
2024-10-10 00:56:05 -0300

    add login html

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
 
+  email: string = "";
+  password: string = "";
+
+  login() {
+    console.log("Holis que onda");
+    // 1° validar validacion
+
+    // 2° llamar al login backend
+
+    // 3° validar el llamado
+    //if ok entonces navigate a la prox pantalla dependiendo el rol
+    //if mal entonces mostrar error
+  }
 }
