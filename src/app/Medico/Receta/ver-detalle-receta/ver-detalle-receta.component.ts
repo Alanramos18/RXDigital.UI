5d59c6b1c2cdff35c542e026f0917137d375d095
Melina Cruz Aro <103278234+mca614@users.noreply.github.com>
2024-11-06 23:09:42 -0300

    añadí el componente DetalleReceta en ver-detalle-receta

--- src/app/Medico/Receta/ver-detalle-receta/ver-detalle-receta.component.ts
+++ src/app/Medico/Receta/ver-detalle-receta/ver-detalle-receta.component.ts
@@ -2,13 +2,14 @@ import { Component} from '@angular/core';
 import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { EncabezadoComponent } from '../../../encabezado/encabezado.component';
+import { DetalleRecetaComponent } from '../../../DetalleReceta/detalle-receta.component';
 
 
 
 @Component({
   selector: 'app-ver-detalle-receta',
   standalone: true,
-  imports: [CommonModule,EncabezadoComponent], 
+  imports: [CommonModule,EncabezadoComponent, DetalleRecetaComponent], 
   templateUrl: './ver-detalle-receta.component.html',
   styleUrl: './ver-detalle-receta.component.scss'
 })
@@ -16,32 +17,6 @@ import { EncabezadoComponent } from '../../../encabezado/encabezado.component';
 export class VerDetalleRecetaComponent {
 
   constructor(private router: Router) { }
-  
-
-  paciente = {
-    nombre: 'Juan Pérez',
-    dni: '12345678',
-    obraSocial: 'OSDE'
-  };
-
-  medico = {
-    nombre: 'Dra. María López',
-    especialidad: 'Cardiología',
-    matricula: '4567'
-  };
-
-  receta = {
-    diagnostico: 'Hipertensión arterial',
-    comentarios: 'Tomar el medicamento después de las comidas',
-    formaEnvio: 'Email y WhatsApp',
-    medicamentos: [
-      { nombre: 'Medicamento A', presentacion: 'Tableta', concentracion: '500mg', cantidad: 30, indicaciones: 'Tomar 1 tableta cada 8' },
-      { nombre: 'Medicamento B', presentacion: 'Jarabe', concentracion: '100ml', cantidad: 2, indicaciones: 'Tomar 5ml cada 6 horas' }
-    ],
-    codigo: 'AB125',
-     fechaEmision: '14/09/2024',
-     fechaVencimiento: '14/12/2024'
-  };
 
   volver() {
     console.log("Volver a la pantalla anterior");
