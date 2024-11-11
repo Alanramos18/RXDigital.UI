75611de61fb639f1068ee058890fa76b278d24be
Melina Cruz Aro <103278234+mca614@users.noreply.github.com>
2024-11-11 16:59:25 -0300

    act gestionar-usuarios

--- src/app/administrador/gestionar-usuarios/gestionar-usuarios.component.ts
+++ src/app/administrador/gestionar-usuarios/gestionar-usuarios.component.ts
@@ -21,7 +21,7 @@ interface Usuario {
 export class GestionarUsuariosComponent {
   usuariosPendientes: Usuario[] = [
     { nombre: 'Juan', apellido: 'Perez', dni: '23456788', rol: '2', email: 'cuenta1@gmail.com', contrasena: 'hola123', matricula: 'MN1234' },
-    { nombre: 'Maria', apellido: 'Lopez', dni: '42567832', rol: '3', email: 'cuenta2@gmail.com', contrasena: 'hola456', matricula: 'MN3455'},
+    { nombre: 'Maria', apellido: 'Lopez', dni: '42567832', rol: '3', email: 'cuenta2@gmail.com', contrasena: 'hola456', matricula: 'MN3455' },
     { nombre: 'Esteban', apellido: 'Quito', dni: '38238956', rol: '1', email: 'cuenta3@gmail.com', contrasena: 'hola567' },
     // Otros usuarios simulados
   ];
@@ -34,12 +34,9 @@ export class GestionarUsuariosComponent {
   }
 
   rechazarUsuario(usuario: Usuario) {
-    const motivoRechazo = prompt("Ingrese el motivo del rechazo:");
-    if (motivoRechazo) {
-      // Aquí va la lógica para registrar el rechazo del usuario junto con el motivo
-      alert(`Usuario ${usuario.nombre} rechazado por: ${motivoRechazo}`);
-      // Luego eliminar el usuario de la lista o actualizar el estado
-      this.usuariosPendientes = this.usuariosPendientes.filter(u => u !== usuario);
-    }
+    // Aquí va la lógica para registrar el rechazo del usuario junto con el motivo
+    alert(`Usuario ${usuario.nombre} rechazado.`);
+    // Luego eliminar el usuario de la lista o actualizar el estado
+    this.usuariosPendientes = this.usuariosPendientes.filter(u => u !== usuario);
   }
 }
