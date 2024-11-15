ba75d0b159f8ab810b31fb292d44fc032a409943
KarinaFC <karinafamiliacruz@gmail.com>
2024-11-15 18:44:41 -0300

    actRecetasFechas

--- src/app/administrador/consultar/recetas-fecha/recetas-fecha.component.ts
+++ src/app/administrador/consultar/recetas-fecha/recetas-fecha.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { EncabezadoComponent } from '../../../shared/encabezado/encabezado.component';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Receta } from '../../../models/receta';
 
 @Component({
   selector: 'app-recetas-fecha',
   standalone: true,
-  imports: [EncabezadoComponent],
+  imports: [EncabezadoComponent, CommonModule,FormsModule],
   templateUrl: './recetas-fecha.component.html',
   styleUrl: './recetas-fecha.component.scss'
 })
 export class RecetasFechaComponent {
+
+  recetas: Receta[];
+  recetasFiltradas: Receta[];
+  codeFilter = "";
+
+  filterCodes(){}
 
 }
